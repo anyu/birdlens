c046a0ee50a1301f511faf1478d425e90c428504
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-10 17:19:03 -0400

    update motion capture

--- rpi/camera_motion_capture/motion_capture.py
+++ rpi/camera_motion_capture/motion_capture.py
@@ -1,33 +1,34 @@
 from gpiozero import MotionSensor
-# from picamera import PiCamera
 from time import sleep
-from signal import pause
+import datetime
+import subprocess
 
 print("Taking 3 seconds to warm up motion sensor...")
 sleep(3)
 print("Motion sensor should be warmed up!")
 
 pir = MotionSensor(4)
-while True:
-  pir.wait_for_motion()
-  print("Motion detected!")
-  pir.wait_for_no_motion()
-  print("No motion detected")
-
-camera = PiCamera()
-# camera.start_preview()
+camera_process = None
 
 def take_photo():
-  global image
-  i = i + 1
-  camera.capture('image_%s.jpg' % i)
-  print("photo taken")
+  timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+  image_name = f'image_{timestamp}.jpg'
+  camera_capture_cmd = f'libcamera-still -o {image_name}'
+  camera_process = subprocess.Popen(camera_capture_cmd, shell=True)
+  print(f"Photo taken: {image_name}")
   sleep(10)
 
 def stop_camera():
-  camera.stop_preview()
+  if camera_process is not None:
+    camera_process.terminate()
+    camera_process.wait()
+    exit()
+
+pir.when_motion = take_photo
+pir.when_no_motion = stop_camera
+
+try:
+  while True:
+    sleep(1)
+except KeyboardInterrupt:
   exit()
-
-# pir.when_motion = take_photo
-
-pause()
