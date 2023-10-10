72672e602cc0b6b373022e2a4f1f02a214ac95a6
An Yu <1009227+anyu@users.noreply.github.com>
2023-10-10 16:25:57 -0400

    Send email; refactor into classes

--- rpi/app/app.py
+++ rpi/app/app.py
@@ -5,7 +5,10 @@ import numpy as np
 from fastcore.all import *
 from fastai.vision import * 
 from fastai.vision.all import *
+from dotenv import load_dotenv
+from custom_email_sender import EmailSender
 
+load_dotenv()
 print("Script is running...")
 
 RPI_CAM_CAPTURES_PATH = "./camera_captures"
@@ -13,60 +16,81 @@ GLOBAL_FILELIST_PATH = "./global_filelist.txt"
 CLASSIFIED_IMAGES_PATH = "./classified_images"
 ML_MODEL_PATH = "./birdlens_resnet_34_model.pkl"
 
-def check_for_new_images(directory_to_monitor, ml_model_path, prev_images):
-  with open(GLOBAL_FILELIST_PATH, 'r') as file:
-    prev_images = [line.strip() for line in file]
-    print("Previously classified images:", prev_images)
+class ImageClassifier:
+  def __init__(self, model_path):
+    self.model_path = model_path
+    self.learner = load_learner(model_path, cpu=True)
 
-  current_files = set(os.listdir(directory_to_monitor))
-  new_files = current_files - set(prev_images)
-  return new_files
+  def classify_image(self, image_file):
+    image_path = os.path.join(RPI_CAM_CAPTURES_PATH, image_file)
+    img = Image.open(image_path)
+    img_arr = np.asarray(img)
+    prediction = self.learner.predict(img_arr)
+    return prediction
 
-def classify_bird(image_file):
-  image_path = os.path.join(RPI_CAM_CAPTURES_PATH, image_file)
-  img = Image.open(image_path)
-  imgArr = np.asarray(img)
-  learner = load_learner(ML_MODEL_PATH, cpu=True)
+class FileHandler:
+  @staticmethod
+  def update_global_filelist(new_file):
+    print(f"Updating global filelist with new file {new_file}")
+    with open(GLOBAL_FILELIST_PATH, 'a') as file:
+      file.write(new_file + '\n')
 
-  prediction = learner.predict(imgArr)
-  print(f"Image: {image_file}, Predicted Class: {prediction[0]}, Confidence: {prediction[2][prediction[1]].item():.4f}")
+  @staticmethod
+  def move_file(file):
+    if not os.path.exists(CLASSIFIED_IMAGES_PATH):
+      print(f"{CLASSIFIED_IMAGES_PATH} does not exist. Creating...")
+      os.makedirs(CLASSIFIED_IMAGES_PATH)
 
-def update_global_filelist(new_file):
-  print(f"Updating global filelist with new file {new_file}")
-  with open(GLOBAL_FILELIST_PATH, 'a') as file:
-    file.write(new_file + '\n')
+    source_path = os.path.join(RPI_CAM_CAPTURES_PATH, file)
+    destination_path = os.path.join(CLASSIFIED_IMAGES_PATH, file)
 
-def move_file(file):
-  if not os.path.exists(CLASSIFIED_IMAGES_PATH):
-    print(f"{CLASSIFIED_IMAGES_PATH} does not exist. Creating...")
-    os.makedirs(CLASSIFIED_IMAGES_PATH)
+    try:
+      os.rename(source_path, destination_path)
+      print(f"Moved {file} to {CLASSIFIED_IMAGES_PATH}\n")
+    except Exception as e:
+      print(f"Error moving {file}: {str(e)}")
 
-  source_path = os.path.join(RPI_CAM_CAPTURES_PATH, file)
-  destination_path = os.path.join(CLASSIFIED_IMAGES_PATH, file)
+class ImageProcessor:
+  def __init__(self, classifier, file_handler, email_sender):
+    self.classifier = classifier
+    self.file_handler = file_handler
+    self.email_sender = email_sender
 
-  try:
-    os.rename(source_path, destination_path)
-    print(f"Moved {file} to {CLASSIFIED_IMAGES_PATH}\n")
-  except Exception as e:
-    print(f"Error moving {file}: {str(e)}")
+  def process_images(self, prev_images):
+    new_files = set(os.listdir(RPI_CAM_CAPTURES_PATH)) - set(prev_images)
+    for image_file in new_files:
+      prediction_result = self.classifier.classify_image(image_file)
+      source_path = os.path.join(RPI_CAM_CAPTURES_PATH, image_file)
+
+      self.trigger_email(prediction_result, source_path)
+      self.file_handler.update_global_filelist(image_file)
+      self.file_handler.move_file(image_file)
+
+  def trigger_email(self, prediction_result, image_file):
+    print(f"Image: {image_file}, Predicted Class: {prediction_result[0]}, Confidence: {prediction_result[2][prediction_result[1]].item():.4f}")
+    subject = 'Bird detected!'
+    message_body = 'birdlens detected this bird:'
+
+    self.email_sender.send_email(subject, message_body, image_file)
 
 def main():
+  sender_email = os.getenv('SENDER_EMAIL')
+  sender_password = os.getenv('SENDER_PASSWORD')
+  recipient_email = os.getenv('RECIPIENT_EMAIL')
+
+  if not sender_email or not sender_password or not recipient_email:
+    print("Error: Missing required environment variables.")
+    exit(1)
+
+  email_sender = EmailSender(sender_email, sender_password, recipient_email)
+  file_handler = FileHandler()
+  image_classifier = ImageClassifier(ML_MODEL_PATH)
+  image_processor = ImageProcessor(image_classifier, file_handler, email_sender)
+
   prev_files = set()
   while True:
-    new_images = check_for_new_images(RPI_CAM_CAPTURES_PATH, ML_MODEL_PATH, prev_files)
-
-    if new_images:
-      print("New images detected:")
-      for image in new_images:
-        classify_bird(image)
-        update_global_filelist(image)
-        move_file(image)
-    else:
-      print("No new images detected.\n")
-    
+    image_processor.process_images(prev_files)
     time.sleep(10)  
 
 if __name__ == "__main__":
-	main()
- 
-
+  main()
---
+++ rpi/app/custom_email_sender.py
@@ -0,0 +1,41 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
+
+class EmailSender:
+  def __init__(self, sender_email, sender_password, recipient_email):
+    self.sender_email = sender_email
+    self.sender_password = sender_password
+    self.recipient_email = recipient_email
+
+  def send_email(self, subject, message_body, image_path):
+    smtp_server = 'smtp.mail.yahoo.com'
+    smtp_port = 587
+
+    msg = MIMEMultipart()
+    msg['From'] = self.sender_email
+    msg['To'] = self.recipient_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message_body, 'plain'))
+    attachment = open(image_path, 'rb')
+    image = MIMEImage(attachment.read(), name='image.jpg')
+    attachment.close()
+    msg.attach(image)
+
+    try:
+      server = smtplib.SMTP(smtp_server, smtp_port)
+      server.starttls()
+      server.login(self.sender_email, self.sender_password)
+
+      server.sendmail(self.sender_email, self.recipient_email, msg.as_string())
+      print("Email sent successfully!")
+
+    except Exception as e:
+      print(f"Error: {str(e)}")
+
+    finally:
+      server.quit()
